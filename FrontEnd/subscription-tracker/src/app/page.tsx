a8c95b3d5a76d13b4ba8739b492182bdd5dea3d2
Shahriar <mshahriar40@gmail.com>
2025-05-15 21:41:43 +0600

    Version: 0.7 Activity:
    - Frontend (NextJS) Project started and folder structure implemented.

---
+++ FrontEnd/subscription-tracker/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p className="text-2xl">Welcome to the Subscription Tracker</p>
+    </div>
+  );
+}
