272bbfaf4a101eaadf1cb1e88c8db47db11da425
Shahriar <mshahriar40@gmail.com>
2025-07-03 14:22:00 +0600

    Activity: Docker - TZ env variable added in frontend dockerFile. - Mysql client used for check migration - main entrypoint updated.

--- Backend/SubscriptionTracker/docker/entrypoint.sh
+++ Backend/SubscriptionTracker/docker/entrypoint.sh
@@ -2,29 +2,31 @@
 
 set -e
 
-# Replace .env with .env.docker always
-# echo "Overwriting .env with .env.production..."
-# rm -f .env
-# cp .env.production .env
-
 if [ ! -f .env ]; then
   echo "Copying .env.example to .env..."
   cp .env.example .env
 fi
 
-# Wait for database and Redis
-echo "Waiting for database connection..."
-echo "Running migrations..."
-until php artisan migrate --force; do
-  echo "Migration failed, retrying in 3 seconds..."
+# Wait until MySQL is accepting connections
+echo "Waiting for MySQL to be ready at $DB_HOST:$DB_PORT..."
+until mysqladmin ping -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USERNAME" -p"$DB_PASSWORD" --silent; do
+  echo "MySQL is unavailable - sleeping"
   sleep 3
 done
-echo "Migrations complete."
+echo "MySQL is up - continuing"
 
-echo "Database is ready."
-
-# Run migrations
-php artisan migrate --force
+# Check if migration table exists before migrating
+echo "Checking if migrations table exists..."
+if php artisan migrate:status | grep -q "Migration table not found"; then
+  echo "Migrations not found. Running migrations..."
+  until php artisan migrate --force; do
+    echo "Migration failed, retrying in 3 seconds..."
+    sleep 3
+  done
+  echo "Migrations completed."
+else
+  echo "Migrations already done. Skipping."
+fi
 
 # Cache and optimization
 php artisan optimize:clear
