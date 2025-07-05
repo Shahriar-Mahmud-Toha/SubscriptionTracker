dd3640cc74d6ab4c0c02c4a02e9e47a73c94b0f8
Shahriar <mshahriar40@gmail.com>
2025-07-05 06:06:14 +0600

    Github action updated. Test 1.

--- FrontEnd/subscription-tracker/src/app/(public)/page.tsx
+++ FrontEnd/subscription-tracker/src/app/(public)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="container mb-20 flex flex-col items-center">
       <HomeHero image={heroImage} imgAlt="Subscription Tracker Hero Image" appName="Subscription Tracker" appNameColor="text-custom-violet" heroText="is an online tool designed to help you easily manage your subscription details and receive automatic email reminders before any subscription expires." />
-      <HeroNotes noteText="[Test 1] This tool is created for testing purposes only and is not intended for real-world use." customClasses="my-5" />
+      <HeroNotes noteText="[Test] This tool is created for testing purposes only and is not intended for real-world use." customClasses="my-5" />
       <div className="flex justify-center mt-10">
         <LinkButtonRegular href="/login" text="Get Started" />
       </div>
